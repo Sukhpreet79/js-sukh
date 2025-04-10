93b4b068e882a3dd09f4e9d7de05477900f8e5fc
Sukhpreet Jena <139633990+Sukhpreet79@users.noreply.github.com>
2025-04-10 15:48:41 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3 +3);
+
+console.log("sukhpreet");
+
+let name = "sukhpreet"
+let age = 18
+let inLoggedIn = false
+let state = null;
+
+ //number => 2^53
+ //bigint
+ //string =>""
+ //boolean => true/false
+ //null =>standalone value (represnts empty value)
+ //undefined => value not declare
+// symbol => unique 
+
+//object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined is atype
