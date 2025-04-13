2dc8698ede0a3fc01f46eb30db1962dc7c50a978
Sukhpreet Jena <139633990+Sukhpreet79@users.noreply.github.com>
2025-04-13 17:38:40 +0000

    1_variables

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,6 +28,47 @@ console.log(booleanIsLoggedIn);
 // 1 = true ; 0= false
 // ""= false ; "sukhpreet" = true
 
-let someNumber = 33
+/*let someNumber = 33
 let stringNumber = String(someNumber)
- console.log(stringNumber)
+ console.log(stringNumber)*/
+
+ // ************** OPERATIONS ********************//
+
+ /*let value = 3
+ let negvalue = -value*/
+ //console.log(negvalue);
+
+ /*console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**2);
+ console.log(2/2);
+ console.log(2%2);
+*/
+
+/*let str1 = "hello"
+let str2 = "sukhpreet"
+
+let str3 = str1 + str2
+console.log(str3);*/
+
+/*console.log("1" + 2);
+console.log(1 + "2")
+console.log("1" +2 +2);
+console.log( 1+ 2+ "2");
+// 57.if string is in first then it will treat everyone as treat 
+//  58.if number is at first it will add the number first
+console.log ((3+4) * 5 % 3);*/ //its a confusing code
+
+/*console.log(+true); // 1
+console.log(+""); //0*/
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
