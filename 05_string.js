bafb1d48b8132475e2069ed34a674f8167c9ead8
Sukhpreet Jena <139633990+Sukhpreet79@users.noreply.github.com>
2025-04-17 06:22:54 +0000

    string

--- 05_string.js
+++ 05_string.js
@@ -1,3 +1,6 @@
 const name = "sukhpreet"
 const repoCount = 50
- console.log(name + repoCount + "value");
+
+ //console.log(name + repoCount + "value");
+
+ console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
